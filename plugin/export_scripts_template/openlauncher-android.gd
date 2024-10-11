5c1ab66a89311381021ae08d4fc2aea1bb7ed9cd
woliver99 <oliver@maplenetwork.ca>
2024-10-11 13:54:28 -0400

    Removed unnecessary demo project

--- plugin/export_scripts_template/openlauncher-android.gd
+++ plugin/export_scripts_template/openlauncher-android.gd
@@ -4,39 +4,33 @@ extends EditorPlugin
 # A class member to hold the editor export plugin during its lifecycle.
 var export_plugin : AndroidExportPlugin
 
-func _enter_tree():
+func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
 	export_plugin = AndroidExportPlugin.new()
 	add_export_plugin(export_plugin)
 
-
-func _exit_tree():
+func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
 	remove_export_plugin(export_plugin)
 	export_plugin = null
 
-
 class AndroidExportPlugin extends EditorExportPlugin:
-	# TODO: Update to your plugin's name.
+	# Plugin's name.
 	var _plugin_name = "openlauncher-android"
 
+	# Specifies which platform is supported by the plugin.
 	func _supports_platform(platform):
 		if platform is EditorExportPlatformAndroid:
 			return true
 		return false
 
+	# Return the paths of the plugin's AAR binaries relative to the 'addons' directory.
 	func _get_android_libraries(platform, debug):
 		if debug:
-			return PackedStringArray([_plugin_name + "/bin/debug/" + _plugin_name + "-debug.aar"])
+			return PackedStringArray(["openlauncher-android/openlauncher-android-debug.aar"])
 		else:
-			return PackedStringArray([_plugin_name + "/bin/release/" + _plugin_name + "-release.aar"])
+			return PackedStringArray(["openlauncher-android/openlauncher-android.aar"])
 
-	func _get_android_dependencies(platform, debug):
-		# TODO: Add remote dependices here.
-		if debug:
-			return PackedStringArray([])
-		else:
-			return PackedStringArray([])
-
+	# Return the plugin's name.
 	func _get_name():
 		return _plugin_name
